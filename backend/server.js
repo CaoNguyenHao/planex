4f8a3d306256be253890677ab652a500a0dadc39
Hao CN <caonguyenhao69@gmail.com>
2025-09-13 17:31:28 +0700

    Ready to deploy

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-const app = express()
-
-app.listen(5001, () => {
-    console.log('Server listen on port 5001')
-})
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,36 @@
-import express from 'express'
-const app = express()
-import taskRoute from './routes/tasksRouters.js'
-import {connectDB} from './config/db.js'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import taskRoute from "./routes/tasksRouters.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
 
-dotenv.config()
-const PORT = process.env.PORT || 5001
+dotenv.config();
 
-//middlewares
-app.use(express.json())
-app.use(cors({origin: 'http://localhost:5173'}))
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use("/api/tasks",taskRoute)
+const app = express();
+
+// middlewares
+app.use(express.json());
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+
+app.use("/api/tasks", taskRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-    console.log(`Server listen on port ${PORT}`)
-    })
-})
+  app.listen(PORT, () => {
+    console.log(`Server bắt đầu trên cổng ${PORT}`);
+  });
+});
